55e46e2a24f5ab4af097d3febab94710be0dd063
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-03-30 15:10:06 +0200

    first commit

---
+++ public/posts/postsList.js
@@ -0,0 +1,49 @@
+const posts = [
+    {
+        titolo: 'Ciambellone',
+        contenuto: 'Ricetta',
+        Img: 'images/ciambellone.jpeg',
+        tags: [
+            'Mio blog',
+            'Mie ricette'
+        ]
+    },
+    {
+        titolo: 'Cracker barbabietola',
+        contenuto: 'Ricetta',
+        Img: 'images/cracker_barbabietola.jpeg',
+        tags: [
+            'Mio blog',
+            'Mie ricette'
+        ]
+    },
+    {
+        titolo: 'Pane fritto dolce',
+        contenuto: 'Ricetta',
+        Img: 'images/pane_fritto_dolce.jpeg',
+        tags: [
+            'Mio blog',
+            'Mie ricette'
+        ]
+    },
+    {
+        titolo: 'Pasta barbabietola',
+        contenuto: 'Ricetta',
+        Img: 'images/pasta_barbabietola.jpeg',
+        tags: [
+            'Mio blog',
+            'Mie ricette'
+        ]
+    },
+    {
+        titolo: 'Torta paesana',
+        contenuto: 'Ricetta',
+        Img: 'images/torta_paesana.jpeg',
+        tags: [
+            'Mio blog',
+            'Mie ricette'
+        ]
+    }
+];
+
+module.exports = posts;
---
+++ routers/posts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.router();
+const posts = require('./../public/posts/postsList');
+
+
+
+//index
+router.get('/', function (req, res) {
+    res.json(posts);
+});
+
+// show
+router.get('/:id', function (req, res) {
+    res.send('Dettagli del posts ' + req.params.id);
+});
+
+// store
+router.post('/', function (req, res) {
+    res.send('Creazione nuovo posts');
+});
+
+// update
+router.put('/:id', function (req, res) {
+    res.send('Modifica del post' + req.params.id);
+});
+
+// modify
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale del post ' + req.params.id);
+});
+
+// destroy
+router.delete('/:id', function (req, res) {
+    res.send('Cancellazione del post ' + req.params.id);
+});
+
+module.exports = router;
